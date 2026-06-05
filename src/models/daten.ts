ea107a76cff1e682fe5e2250e9a51cf4e1d43398
neo849 <134264402+NEO849@users.noreply.github.com>
2026-06-05 05:00:02 +0200

    copy(projekte): Z-Almanach-App - GitHub-Link-Button entfernt

--- src/models/daten.ts
+++ src/models/daten.ts
@@ -192,7 +192,6 @@ export const PROJEKTE: ProjektModel[] = [
       "Flüssige Listen auch bei sehr vielen Einträgen",
       "Merkt sich Einstellungen und Zustand zwischen App-Starts",
     ],
-    linkGithub: "https://github.com/NEO849/ZAlmanach",
     zeitraum: "2023",
     status: "abgeschlossen",
   },
